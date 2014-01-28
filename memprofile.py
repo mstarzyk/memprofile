d809656256133a0541c6908ecc15aab5e6558a3e
Maciek Starzyk <mstarzyk@gmail.com>
2014-01-28 21:58:31 +0100

    Return code, time formatting.

--- memprofile.py
+++ memprofile.py
@@ -2,20 +2,21 @@ from __future__ import print_function
 import subprocess
 import time
 import argparse
+import sys
 
 def sample(pid):
     fname = "/proc/{}/statm".format(pid)
     with open(fname) as f:
         line = f.readline()
         stat = [int(x) for x in line.split()]
-        return {'size': stat[0], 'resident': stat[1]}
+        ts = "{:0.3f}".format(time.time())
+        return {'time': ts, 'size': stat[0], 'resident': stat[1]}
 
 def main(sampling_time, command):
     print("Running {}".format(command))
-    # proc = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     with open("/dev/null", 'w') as dev_null:
-        proc = subprocess.Popen(command, stdout=dev_null, stderr=dev_null)
-        print("Running process with PID={}".format(proc.pid))
+        proc = subprocess.Popen(command, stdout=dev_null, stderr=subprocess.STDOUT)
+        print("PID={}".format(proc.pid))
         start = time.time()
         now = start
         while True:
@@ -25,13 +26,10 @@ def main(sampling_time, command):
                 time.sleep(sampling_time)
             else:
                 now = time.time()
-                latency = now - start
-                print("Process finished, retcode={}, latency={}.".format(returncode, latency))
+                elapsed = now - start
+                print("Process finished, retcode={0}, time={1:0.3f}".format(returncode, elapsed))
                 break
-    # now - start
-
-    # print(stdout)
-    # print(stderr, file=sys.stderr)
+    return returncode
 
 
 
@@ -42,11 +40,11 @@ def parse_configuration():
             )
 
     parser.add_argument("-t", dest="sampling_time", help='Sampling time (seconds, default 1.0)', default=1.0, type=float)
-    parser.add_argument("-c", dest="command", help='Command.', nargs=argparse.REMAINDER)
+    parser.add_argument("-c", dest="command", help='Command.', nargs=argparse.REMAINDER, required=True)
 
     return parser.parse_args()
 
 
 if __name__ == "__main__":
     config = parse_configuration()
-    main(**vars(config))
+    sys.exit(main(**vars(config)))
